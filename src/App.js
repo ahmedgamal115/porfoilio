759646e567b17f3b90179fb724a08b5bc9d88d51
ahmedgamal115 <ahmedgamal1153gmail.com>
2022-09-08 02:19:26 +0200

    Create banner page with his style but not complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './component/Navbar'
+import Banner from './component/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Banner/>
     </div>
   );
 }
